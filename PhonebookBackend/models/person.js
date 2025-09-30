433782eae6367bfcc0e0a80473025c2e3e0c0925
BookStore Developer <zznttpzz@gmail.com>
2025-10-01 02:04:50 +0700

    update

--- PhonebookBackend/models/person.js
+++ PhonebookBackend/models/person.js
@@ -23,14 +23,10 @@ const personSchema = new mongoose.Schema(
     number: {
       type: String,
       required: true,
-      minLength: 8,
-      validate: {
-        validator: function (v) {
-          return /\d{2,3}-\d{7}/.test(v);
-        }
-      }
+      minLength: 5
     }
-  })
+  }
+)
 personSchema.set('toJSON', {
   transform(document, returnedObject) {
     returnedObject.id = returnedObject._id.toString();
